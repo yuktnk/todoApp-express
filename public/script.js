9876080c11fd2fff04e1a8310abbaec0faf2a86e
yuktnk <yuki0wl@gmail.com>
2022-09-10 14:28:26 +0900

    タスクの削除、タスクが１件もない時のハンドリング

--- public/script.js
+++ public/script.js
@@ -1,11 +1,19 @@
+const taskList = document.querySelector(".tasks");
+const formDOM = document.querySelector(".task-form");
+const taskInputDOM = document.querySelector(".task-input");
+
 /**
  * タスクの読み込み
  */
-
-const taskList = document.querySelector(".tasks");
 const showTasks = async () => {
     try {
         const { data: tasks } = await axios.get("/api/v1/tasks");
+
+        // タスクがひとつもないとき
+        if (tasks.length < 1) {
+            taskList.innerHTML = `<h5 class="empty-list">全てのタスクを完了しました</h5>`;
+            reuturn;
+        }
 
         const allTasks = tasks
             .map((task) => {
@@ -21,7 +29,7 @@ const showTasks = async () => {
                                     <i class="fas fa-edit"></i>
                                 </a>
                                 <!-- ゴミ箱リンク -->
-                                <button type="button" class="delete-btn">
+                                <button type="button" class="delete-btn" data-id="${_id}">
                                     <i class="fas fa-trash"></i>
                                 </button>
                             </div>
@@ -39,8 +47,6 @@ showTasks();
 /**
  * タスクの作成
  */
-const formDOM = document.querySelector(".task-form");
-const taskInputDOM = document.querySelector(".task-input");
 formDOM.addEventListener("submit", async (e) => {
     e.preventDefault();
     const name = taskInputDOM.value;
@@ -51,5 +57,23 @@ formDOM.addEventListener("submit", async (e) => {
         taskInputDOM.value = "";
     } catch (error) {
         console.log(error);
+    }
+});
+
+/**
+ * タスクの削除、編集
+ */
+taskList.addEventListener("click", async (e) => {
+    const element = e.target;
+
+    // タスクの削除
+    if (element.parentElement.classList.contains("delete-btn")) {
+        const id = element.parentElement.dataset.id;
+        try {
+            await axios.delete(`/api/v1/tasks/${id}`);
+            showTasks();
+        } catch (error) {
+            console.log(error);
+        }
     }
 });
